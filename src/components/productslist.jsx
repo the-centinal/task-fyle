d2737fd14fc38556772d2544fd549caf1becee2e
the-centinal <praveenkyadav02@gmail.com>
2022-09-28 00:23:14 +0530

    added search function

--- src/components/productslist.jsx
+++ src/components/productslist.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import Fuse from 'fuse.js';
 import Skeleton from 'react-loading-skeleton'
 import { NavLink } from 'react-router-dom'
 
+
 function Productslist() {
+
+
+    const [query, setQuery] = useState('');
+
     const [data, setData] = useState([]);
     // const [filter, setFilter] = useState(data);
     const [loading, setLoading] = useState(false);
@@ -26,6 +32,21 @@ function Productslist() {
     }, [])
 
 
+    const fuse = new Fuse(data, {
+        keys: [
+            'login'
+        ],
+
+    })
+    const results = fuse.search(query);
+    const searchedData = query ? results.map(result => result.item) : data;
+
+    function handleSearch({currentTarget = {}}) {
+        const { value } = currentTarget;
+        setQuery(value)
+        
+    }
+
     const Loading = () => {
         return (
             <div>
@@ -43,21 +64,17 @@ function Productslist() {
                 </div>
             </div>
         );
-
-
-
-
     };
 
-   
+
 
 
     const ShowProducts = () => {
         return (
             <>
 
-                
-                {data.map((product) => {
+
+                {searchedData.map((product) => {
                     return (
                         <>
                             <div className="col-md-3 mb-4">
@@ -66,8 +83,8 @@ function Productslist() {
                                     <div class="card-body">
                                         <h5 className="card-title mb-0">{product.login.substring(0, 100)}</h5>
                                         <br />
-                                        {/*<p className="card-text lead fw-bold">$ {product.price}</p>*/   }
-                                        
+                                        {/*<p className="card-text lead fw-bold">$ {product.price}</p>*/}
+
                                         <NavLink to={`/users/${product.login}/repos`} className="btn btn-outline-dark">SHOW REPOS</NavLink>
                                     </div>
                                 </div>
@@ -80,14 +97,24 @@ function Productslist() {
     }
 
     return (
-        <div>
-            <div className="container my-5 py-5">
-                
-                <div className="row justify-content-center">
-                    {loading ? <Loading /> : <ShowProducts />}
+        <>
+            <nav className="navbar bg-light" style={{marginTop : "5%" }}>
+                <div className="container-fluid">
+                    <form clasNames="d-flex" role="search" style={{display: "flex", margin: "auto"}}>
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={query} onChange={handleSearch} />
+                           
+                    </form>
+                </div>
+            </nav>
+            <div>
+                <div className="container my-5 py-5">
+
+                    <div className="row justify-content-center">
+                        {loading ? <Loading /> : <ShowProducts />}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
